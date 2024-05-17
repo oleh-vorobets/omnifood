391ada4c4615854b1184efe5b508ab3003c11ff8
Oleh Vorobets <olehcooperation@gmail.com>
2024-05-18 00:09:09 +0300

    Final version but without images optimization

--- js/script.js
+++ js/script.js
@@ -35,3 +35,24 @@ allLinks.forEach((link) => {
         }
     });
 });
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+        if (!ent.isIntersecting) {
+            document.body.classList.add('sticky');
+        } else {
+            document.body.classList.remove('sticky');
+        }
+    },
+    {
+        root: null,
+        threshold: 0,
+        rootMargin: '-80px',
+    }
+);
+obs.observe(sectionHeroEl);
